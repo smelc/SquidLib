e03dd660bea8c53473e6e81e5983c3873e44e63a
smelc <hgamesdeveloper@gmail.com>
2017-10-14 21:58:44 +0200

    Remove stuff I don't use and which I didn't write in IColorCenter (adapted SquidColorCenter because I cannot get rid of it yet)

--- squidlib-util/src/main/java/squidpony/IColorCenter.java
+++ squidlib-util/src/main/java/squidpony/IColorCenter.java
@@ -125,16 +125,6 @@ public interface IColorCenter<T> {
 	int getAlpha(T c);
 
 	/**
-	 *
-	 * @param c
-	 *            a concrete color
-	 * @return the value (essentially lightness) of the color from 0.0 (black,
-	 *         inclusive) to 1.0 (inclusive) for screen colors or arbitrarily high
-	 *         for HDR colors.
-	 */
-	float getValue(T c);
-
-	/**
 	 * @param c
 	 * @return The color that {@code this} shows when {@code c} is requested. May be
 	 *         {@code c} itself.
@@ -184,46 +174,6 @@ public interface IColorCenter<T> {
 			this.filter = filter;
 		}
 
-		/**
-		 * It clears the cache. You may need to do this to limit the cache to the colors
-		 * used in a specific section. This is also useful if a Filter changes what
-		 * colors it should return on a frame-by-frame basis; in that case, you can call
-		 * clearCache() at the start or end of a frame to ensure the next frame gets
-		 * different colors.
-		 */
-		public void clearCache() {
-			cache.clear();
-		}
-
-		/**
-		 * You may want to copy colors between IColorCenter instances that have
-		 * different create() methods -- and as such, will have different values for the
-		 * same keys in the cache. This allows you to copy the cache from other into
-		 * this Skeleton, but using this Skeleton's create() method.
-		 * 
-		 * @param other
-		 *            another Skeleton of the same type that will have its cache copied
-		 *            into this Skeleton
-		 */
-		public void copyCache(Skeleton<T> other) {
-			for (Map.Entry<Long, T> k : other.cache.entrySet()) {
-				cache.put(k.getKey(), create(getRed(k.getValue()), getGreen(k.getValue()), getBlue(k.getValue()),
-						getAlpha(k.getValue())));
-			}
-		}
-
-		/**
-		 * If you're changing the filter, you should likely call {@link #clearCache()}.
-		 * 
-		 * @param filter
-		 *            The filter to use, or {@code null} to turn filtering OFF.
-		 * @return {@code this}
-		 */
-		public Skeleton<T> setFilter(IFilter<T> filter) {
-			this.filter = filter;
-			return this;
-		}
-
 		@Override
 		public T get(int red, int green, int blue, int opacity) {
 			final Long value = getUniqueIdentifier((short) red, (short) green, (short) blue, (short) opacity);
@@ -260,106 +210,6 @@ public interface IColorCenter<T> {
 		@Override
 		public final T getRandom(RNG rng, int opacity) {
 			return get(rng.nextInt(256), rng.nextInt(256), rng.nextInt(256), opacity);
-		}
-
-		/**
-		 * @param r
-		 *            the red component in 0.0 to 1.0 range, typically
-		 * @param g
-		 *            the green component in 0.0 to 1.0 range, typically
-		 * @param b
-		 *            the blue component in 0.0 to 1.0 range, typically
-		 * @return the saturation of the color from 0.0 (a grayscale color; inclusive)
-		 *         to 1.0 (a bright color, exclusive)
-		 */
-		public float getSaturation(float r, float g, float b) {
-			float min = Math.min(Math.min(r, g), b); // Min. value of RGB
-			float max = Math.max(Math.max(r, g), b); // Min. value of RGB
-			float delta = max - min; // Delta RGB value
-
-			float saturation;
-
-			if (delta < 0.0001f) // This is a gray, no chroma...
-			{
-				saturation = 0;
-			} else // Chromatic data...
-			{
-				saturation = delta / max;
-			}
-			return saturation;
-		}
-
-		/**
-		 * @param r
-		 *            the red component in 0.0 to 1.0 range, typically
-		 * @param g
-		 *            the green component in 0.0 to 1.0 range, typically
-		 * @param b
-		 *            the blue component in 0.0 to 1.0 range, typically
-		 * @return the value (essentially lightness) of the color from 0.0 (black,
-		 *         inclusive) to 1.0 (inclusive) for screen colors or arbitrarily high
-		 *         for HDR colors.
-		 */
-		public float getValue(float r, float g, float b) {
-			return Math.max(Math.max(r, g), b);
-		}
-
-		/**
-		 * @param c
-		 *            a concrete color
-		 * @return the value (essentially lightness) of the color from 0.0 (black,
-		 *         inclusive) to 1.0 (inclusive) for screen colors or arbitrarily high
-		 *         for HDR colors.
-		 */
-		@Override
-		public float getValue(T c) {
-			float r = getRed(c) / 255f; // RGB from 0 to 255
-			float g = getGreen(c) / 255f;
-			float b = getBlue(c) / 255f;
-
-			return Math.max(Math.max(r, g), b);
-		}
-
-		/**
-		 * @param r
-		 *            the red component in 0.0 to 1.0 range, typically
-		 * @param g
-		 *            the green component in 0.0 to 1.0 range, typically
-		 * @param b
-		 *            the blue component in 0.0 to 1.0 range, typically
-		 * @return The hue of the color from 0.0 (red, inclusive) towards orange, then
-		 *         yellow, and eventually to purple before looping back to almost the
-		 *         same red (1.0, exclusive)
-		 */
-		public float getHue(float r, float g, float b) {
-			float min = Math.min(Math.min(r, g), b); // Min. value of RGB
-			float max = Math.max(Math.max(r, g), b); // Min. value of RGB
-			float delta = max - min; // Delta RGB value
-
-			float hue;
-
-			if (delta < 0.0001f) // This is a gray, no chroma...
-			{
-				hue = 0; // HSV results from 0 to 1
-			} else // Chromatic data...
-			{
-				float rDelta = (((max - r) / 6f) + (delta / 2f)) / delta;
-				float gDelta = (((max - g) / 6f) + (delta / 2f)) / delta;
-				float bDelta = (((max - b) / 6f) + (delta / 2f)) / delta;
-
-				if (r == max)
-					hue = bDelta - gDelta;
-				else if (g == max)
-					hue = (1f / 3f) + rDelta - bDelta;
-				else
-					hue = (2f / 3f) + gDelta - rDelta;
-
-				if (hue < 0)
-					hue += 1f;
-				else if (hue > 1)
-					hue -= 1;
-			}
-			return hue;
 		}
 
 		@Override
@@ -433,46 +283,6 @@ public interface IColorCenter<T> {
 				newAlpha = alpha;
 			}
 			return get(mean, mean, mean, newAlpha);
-		}
-
-		/**
-		 * Gets the linear interpolation from Color start to Color end, changing by the
-		 * fraction given by change. This implementation tries to work with colors in a
-		 * way that is as general as possible, using getRed() instead of some specific
-		 * detail that depends on how a color is implemented. Other implementations that
-		 * specialize in a specific type of color may be able to be more efficient.
-		 * 
-		 * @param start
-		 *            the initial color T
-		 * @param end
-		 *            the "target" color T
-		 * @param change
-		 *            the degree to change closer to end; a change of 0.0f produces
-		 *            start, 1.0f produces end
-		 * @return a new T between start and end
-		 */
-		public T lerp(T start, T end, float change) {
-			if (start == null || end == null)
-				return null;
-			final int sr = getRed(start), sg = getGreen(start), sb = getBlue(start), sa = getAlpha(start),
-					er = getRed(end), eg = getGreen(end), eb = getBlue(end), ea = getAlpha(end);
-			return get((int) (sr + change * (er - sr)), (int) (sg + change * (eg - sg)),
-					(int) (sb + change * (eb - sb)), (int) (sa + change * (ea - sa)));
-		}
-
-		/**
-		 * Gets a fully-desaturated version of the given color (keeping its brightness,
-		 * but making it grayscale). Keeps alpha the same; if you want alpha to be
-		 * considered (and brightness to be calculated differently), then you can use
-		 * greify() in this class instead.
-		 * 
-		 * @param color
-		 *            the color T to desaturate (will not be modified)
-		 * @return the grayscale version of color
-		 */
-		public T desaturated(T color) {
-			int f = (int) Math.min(255, getRed(color) * 0.299f + getGreen(color) * 0.587f + getBlue(color) * 0.114f);
-			return get(f, f, f, getAlpha(color));
 		}
 
 		/**
